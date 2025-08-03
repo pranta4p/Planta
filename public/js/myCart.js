f0a15a8b6566f25bbd5355b27a05f2938d8de48f
Soccho27 <2021331027@student.sust.edu>
2025-08-03 14:35:08 +0600

    buy alert added

--- public/js/myCart.js
+++ public/js/myCart.js
@@ -47,13 +47,13 @@ function renderProducts(productsToRender) {
                         <span>Seller: ${product.seller}</span>
                     </div>
                 </div>
-                <button class="add-to-cart-btn" onclick="deleteCart('${product._id}')">
+                <button class="add-to-cart-btn" onclick="alert('${product._id}')">
                     <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-1.1 5.1a1 1 0 001 1.1h9.2a1 1 0 001-1.1L16 13m-5 3v4m2-4v4"></path>
                     </svg>
                     Delete From Cart
                 </button>
-                <button class = "buyNow">
+                <button class = "buyNow" onclick="alert('Cash On Delivery Accepted')">
                     Buy Now
                 </button>
             </div>
